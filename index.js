7801f9d59570d42e9963d874e6052eb2b97f51eb
Isaías Oliveira dos Reis <isaiasodr@gmail.com>
2022-07-25 11:23:07 -0300

    projeto finalizado

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const ProductsRoutes = require('./routes/productsRoutes')
 const app = express()
 
 app.engine('handlebars', exphbs.engine())
+app.set("view engine","handlebars")
 
 
 
@@ -20,4 +21,7 @@ app.use(express.json())
 app.use(express.static(__dirname+'/public'))
 
 app.use("/products", ProductsRoutes)
+app.get("/",(req,res)=>{
+    res.render("home")
+})
 app.listen(3000)
